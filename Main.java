9d5e6933e4d1c624188718d839a9929d82543562
Hugo Notario <hnotariof@esliceu.net>
2022-04-01 16:09:23 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("2 + 4 = " + add(2, 4));
+    }
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+}
